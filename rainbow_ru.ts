037cae493555c70ddb54ffa113aae2084f1526d1
Grigory Ustinov <grenka@altlinux.org>
2023-04-09 21:47:58 +0300

    Update russian translation

--- rainbow_ru.ts
+++ rainbow_ru.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>Rainbow demonstration</source>
-        <translation type="unfinished"></translation>
+        <translation>Демонстрация радуги</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="36"/>
         <source>Page name</source>
-        <translation type="unfinished"></translation>
+        <translation>Название страницы</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="81"/>
@@ -42,34 +42,34 @@
         <location filename="mainwindow.ui" line="128"/>
         <location filename="mainwindow.ui" line="308"/>
         <source>Impact parameter</source>
-        <translation type="unfinished"></translation>
+        <translation>Прицельный параметр</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="177"/>
         <location filename="mainwindow.ui" line="363"/>
         <location filename="mainwindow.ui" line="462"/>
         <source>Wavelength</source>
-        <translation type="unfinished"></translation>
+        <translation>Длина волны</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="224"/>
         <source>Add beam</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить луч</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="233"/>
         <source>&lt; Beam tracking</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt; Ход луча</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="240"/>
         <source>Beam tracking &gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Ход луча &gt;</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="255"/>
         <source>Show all beams</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать все лучи</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="265"/>
@@ -77,7 +77,7 @@
         <location filename="mainwindow.ui" line="561"/>
         <location filename="mainwindow.ui" line="732"/>
         <source>Primary rainbow</source>
-        <translation type="unfinished"></translation>
+        <translation>Первичная радуга</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="272"/>
@@ -85,64 +85,64 @@
         <location filename="mainwindow.ui" line="571"/>
         <location filename="mainwindow.ui" line="739"/>
         <source>Secondary rainbow</source>
-        <translation type="unfinished"></translation>
+        <translation>Вторичная радуга</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="282"/>
         <source>Clear screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Очистить экран</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="436"/>
         <location filename="mainwindow.ui" line="595"/>
         <source>Reset settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Сбросить настройки</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="511"/>
         <source>Number of beams</source>
-        <translation type="unfinished"></translation>
+        <translation>Количество лучей</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="581"/>
         <source>Show angle</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать угол</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="588"/>
         <source>Show rainbow beams</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать лучи радуги</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="625"/>
         <source>All beams</source>
-        <translation type="unfinished"></translation>
+        <translation>Все лучи</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="635"/>
         <source>Observed beams</source>
-        <translation type="unfinished"></translation>
+        <translation>Наблюдаемые лучи</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="642"/>
         <source>Beams from regions of observation</source>
-        <translation type="unfinished"></translation>
+        <translation>Лучи из областей наблюдения</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="654"/>
         <source>Frames per second</source>
-        <translation type="unfinished"></translation>
+        <translation>Кадров в секунду</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="692"/>
         <location filename="mainwindow.cpp" line="331"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Пауза</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="722"/>
         <source>Both rainbows</source>
-        <translation type="unfinished"></translation>
+        <translation>Обе радуги</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="774"/>
@@ -177,32 +177,32 @@
     <message>
         <location filename="mainwindow.cpp" line="92"/>
         <source>Beams in a droplet</source>
-        <translation type="unfinished"></translation>
+        <translation>Ход луча в капле</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="95"/>
         <source>Beam divergence</source>
-        <translation type="unfinished"></translation>
+        <translation>Расходимость лучей</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="98"/>
         <source>Descartes&apos; rainbow</source>
-        <translation type="unfinished"></translation>
+        <translation>Радуга Декарта</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="101"/>
         <source>Ensemble of droplets</source>
-        <translation type="unfinished"></translation>
+        <translation>Ансамбль капель</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="104"/>
         <source>Alexander&apos;s dark band</source>
-        <translation type="unfinished"></translation>
+        <translation>Полоса Александра</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="333"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Старт</translation>
     </message>
 </context>
 <context>
@@ -210,37 +210,37 @@
     <message>
         <location filename="settingswindow.ui" line="14"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="51"/>
         <source>Program language</source>
-        <translation type="unfinished"></translation>
+        <translation>Язык приложения</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="64"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="77"/>
         <source>Start fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>Запускать в полном экране</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="91"/>
         <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation>Тёмная</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="96"/>
         <source>Light</source>
-        <translation type="unfinished"></translation>
+        <translation>Светлая</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="110"/>
         <source>Program theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Тема программы</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="123"/>
@@ -250,7 +250,7 @@
     <message>
         <location filename="settingswindow.ui" line="137"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет</translation>
     </message>
     <message>
         <location filename="settingswindow.ui" line="142"/>
@@ -279,7 +279,7 @@
         <location filename="settingswindow.cpp" line="34"/>
         <location filename="settingswindow.cpp" line="38"/>
         <source>Can&apos;t change language.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не получается изменить язык.</translation>
     </message>
 </context>
 </TS>
