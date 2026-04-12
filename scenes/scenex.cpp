b3b975fab8e7859fcba5249250ccefb6dd7cd46c
Grigory Ustinov <grenka@altlinux.org>
2026-04-12 19:57:25 +0300

    Add axes, water wall and ground surface

--- scenes/scenex.cpp
+++ scenes/scenex.cpp
@@ -19,7 +19,37 @@ void SceneX::display()
         glVertex3d( -MAX, i, 0.0);
         glVertex3d( MAX, i, 0.0);
     }
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3d( 0, -MAX, 0.0);
+    glVertex3d( 0, MAX, 0.0);
+    glVertex3d( 0, 0, -MAX);
+    glVertex3d( 0, 0, MAX);
+    glVertex3d( -MAX, 0, 0.0);
+    glVertex3d( MAX, 0, 0.0);
+
     glEnd();
+
+    // water wall
+    glColor3f(0.7f, 0.7f, 1.0f);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex3d( 0, 0, 0);
+    glVertex3d( 0, -10, -2);
+    glVertex3d( 0, -10, 10);
+    glVertex3d( 0, 10, 10);
+    glVertex3d( 0, 10, -2);
+    glVertex3d( 0, -10, -2);
+    glEnd();
+
+    // surface
+    glColor3f(0.3f, 0.3f, 0.3f);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex3d( 0, 0, 0);
+    glVertex3d( 0, -10, 0);
+    glVertex3d( -20, -10, 0);
+    glVertex3d( -20, 10, 0);
+    glVertex3d( 0, 10, 0);
+    glEnd();
+
 }
 
 void SceneX::updateXY(int newX, int newY)
